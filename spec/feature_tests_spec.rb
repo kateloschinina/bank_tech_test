5d209832338d4b56891d887437d27ec3f3a8af87
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-6.local>
2017-03-06 15:04:47 +0000

    Completed User Story 2.
    As a client of a bank,
    So that I can get some money from my bank account
    I would like to be able to make a withdrawal from my account.
    
    Red / Green / Refactoring. Added withdraw method to Account model.

--- spec/feature_tests_spec.rb
+++ spec/feature_tests_spec.rb
@@ -1,15 +1,27 @@
 require 'account'
 
-describe 'Feature tests for User Stories' do
-  context 'User Story One:' do
+describe "Feature tests for User Stories" do
+  context "User Story One:" do
     # As a client of a bank,
     # So that I can put money on my bank account
     # I would like to be able to deposit my account.
 
-    it 'As a client of a bank, so that I can put money on my bank account I would like to be able to deposit my account.' do
+    it "As a client of a bank, so that I can put money on my bank account I would like to be able to deposit my account." do
       account = Account.new
       account.deposit(1000)
       expect(account.get_balance).to eq(1000)
+    end
+  end
+
+  context "User Srory Two:" do
+    # As a client of a bank,
+    # So that I can get some money from my bank account
+    # I would like to be able to make a withdrawal from my account.
+
+    it "As a client of a bank, so that I can get some money from my bank account I would like to be able to make a withdrawal from my account." do
+      account = Account.new(1000)
+      account.withdraw(500)
+      expect(account.get_balance).to eq(500)
     end
   end
 end
